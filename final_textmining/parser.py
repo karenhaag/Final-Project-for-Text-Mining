d8ca741ddfa96496fef1eaf12ae54b6d8e135f4f
karenhaag <karenhaag91@gmail.com>
2017-11-28 11:12:03 -0300

    finish parser

--- final_textmining/parser.py
+++ final_textmining/parser.py
@@ -41,7 +41,8 @@ def cleanTweetCorpus(path="general-tweets-test.xml"):
 
 
 
-#limpia el corpus de tweeters de hashtag, mensiones, signos de puntuación, etc
+#limpia el corpus de tweeters de hashtag, mensiones, signos de puntuación, etc. 
+#tambien pasa las palabras a lowercase
 def preproces_tweets():
     exclude = string.punctuation
     exclude = exclude + "?" + "”" + "¡" + "¿"
@@ -61,7 +62,7 @@ def preproces_tweets():
         for word in sentence:
             s = ''.join(ch for ch in word if ch not in exclude)
             if s != '':
-                sen.append(s)
+                sen.append(s.lower())
         list_clean_tweets.append(sen)
     return(list_clean_tweets)
 
@@ -78,7 +79,8 @@ def w_after(sent, i):
         return "[end]"
 
 
-#return a list with all words of a sentence
+#return a dic with (key=word, value=index in index_words), a index_words list and
+# a list of context for each word
 def build_training_group(sentences, option_context=1):
     wordsDic = {}
     index_words = []
@@ -109,60 +111,6 @@ def build_training_group(sentences, option_context=1):
                     else:
                         list_context[index][context_after] += 1
     return(wordsDic, index_words, list_context)
-
-
-def feature_selection(dic_context, ocurrences):
-    for context_of_word in dic_contexts:
-        
-        for context in context_of_word:
-            if context_of_word.get(context) < ocurrences:
-                co
-
-
-
-
-def prop1():
-    """
-    - propagación 1:
-    - sobre un corpus, armamos un grafo de co-ocurrencias de las palabras, donde los nodos son las palabras y 
-    existe un arco entre dos nodos si las palabras co-ocurren en un contexto k más de n veces. El arco no es 
-    dirigido y los arcos tienen peso. El peso es alguna función del número de co-ocurrencias de las palabras 
-    (número, tf*idf, IM, etc)
-
-    - colocamos las etiquetas de sentimiento sobre este grafo,
-    y las propagamos con http://scikit-learn.org/stable/modules/label_propagation.html, de forma que las 
-    palabras queden asociadas a clases positivo, negativo y posiblemente neutro (por ejemplo, asociamos una
-    palabra a la etiqueta "neutro" si queda asociada a "positivo" o "negativo" con una intensidad menor a r)
-    """
-
-
-
-def prop2():
-    """
-    - propagación 2:
-    - vemos en un corpus las palabras que están más fuertemente asociadas a palabras de sentimiento positivo o negativo, 
-    mediante Información Mútua, porque ocurren en un contexto k de la palabra semilla.
-    - añadimos a las palabras semilla aquellas que tengan una fuerte Información Mútua
-    - iteramos
-    """
-    sentences = preproces_tweets()
-    wordsDic, index_words, list_context = build_training_group(sentences, 1)
-    with open('positiveWords.pickle', 'rb') as handle:
-        positiveDic = pickle.load(handle)
-    with open('negativeWords.pickle', 'rb') as handle:
-        negativeDic = pickle.load(handle)
-    list_context = feature_selection(list_context)
-    return(wordsDic, index_words, list_context)
-
-
-def prop3():
-    """
-    - propagación 3:
-    - para cada tweet, le asigno clase positiva o negativa usando el clasificador c con el lexicón inicial (semilla)
-    - si un tweet no tiene ninguna palabra ni positiva ni negativa, lo descartamos como ejemplo de aprendizaje
-    - incorporo las palabras que han sido asignadas a oraciones positivas como positivas (valor:1) y a oraciones negativas como negativas (valor: -1). Si una palabra ha sido asignada a ambas clases, le asigno un valor contínuo entre 1 y -1 que sea una función de la proporción de veces que ocurrió en oraciones positivas o negativas
-    - usamos el clasificador con el lexicón aumentado con nuevas palabras para anotar nuevos ejemplos, iteramos
-    """
 
 
 """ 
